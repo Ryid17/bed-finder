559232ec91d63ff3471dd8c2bf2104f8fc42d09b
Ryid Nassif <ryid.nassif@gmail.com>
2020-05-19 11:51:52 +0100

    index done needs styling

--- app/controllers/beds_controller.rb
+++ app/controllers/beds_controller.rb
@@ -1,2 +1,43 @@
 class BedsController < ApplicationController
+  before_action :set_bed, only: [:new, :create, :show, :destroy]
+
+  def index
+    @beds = Bed.all
+  end
+
+  def show
+  end
+
+  def new
+    @bed = Bed.new
+  end
+
+  def create
+    @bed = Bed.new(bed_params)
+    if @bed.save
+      redirect_to beds_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @bed.destroy
+  end
+
+  private
+
+  def set_bed
+    @bed = Bed.find(params[:id])
+  end
+
+  def bed_params
+    params.require(:bed).permit(:category)
+  end
 end
